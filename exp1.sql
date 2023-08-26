94b7fa05c6e7e767fb36bf2618bd2e58ae1a2326
Sneha-priya-A-s <117623763+Sneha-priya-A-s@users.noreply.github.com>
2023-08-26 23:24:40 +0530

    Add files via upload

--- exp1.sql
+++ exp1.sql
@@ -1,45 +1,19 @@
-create database SETOPR;
-use SETOPR;
+create database mcas2;
+use mcas2;
+create table student(
+roll_no int,
+s_name varchar (20),
+m1 int,
+m2 int,
+m3 int);
+desc student;
 
-create table MCA1(
-s1_Rollno int,
-s1_Name char(20),
-Subject char(20));
- 
-create table MCA2(
-s2_Rollno int,
-s2_Name char(20),
-Subject char(20));
- 
-create table MCA3(
-s3_Rollno int,
-s3_Name char(20),
-Subject char(20));
+insert into student value
+(1,'alan',30,35,40),
+(2,'binu',39,50,48);
+select * from student where s_name='alan';
+select * from student where roll_no>3;
+select roll_no,s_name,m1+m2+m3 as mark from student
+where m1+m2+m3>100;
 
-insert into MCA1 values
-(1,'alan','maths'),
-(2,'albin','english'),
-(3,'alan','malayalam'),
-(4,'arjun','maths'),
-(5,'arjun','english');
-
-insert into MCA2 values
-(1,'alan','maths'),
-(2,'albin','english'),
-(3,'albin','malayalam'),
-(4,'arun','maths'),
-(5,'arun','english');
-
-insert into MCA3 values
-(1,'alan','maths'),
-(2,'arun','maths'),
-(3,'yedhu','english'),
-(4,'alan','malayalam'),
-(5,'arjun','english');
-
-select s1_Name from MCA1 union all select s2_Name from MCA2 union all select s3_Name from MCA3;
-select MCA1.s1_Name,MCA2.s2_Name from MCA1,MCA2 where MCA1.s1_Name=MCA2.s2_Name; 
-select s1_Name from MCA1 union all select s2_Name from MCA2;
-select distinct s1_Name from MCA1 union all select s3_Name from MCA3;
-
-select s1_Name from MCA1 intersect select s2_Name from MCA2 
+drop table student;
